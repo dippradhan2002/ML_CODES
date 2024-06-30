27bcbcbd77863e3ceda3afadfe99efd10004063e
Dip Pradhan <117291092+dippradhan2002@users.noreply.github.com>
2024-06-30 18:31:17 +0530

    Update ch_2.2.py

--- Chapter-2/ch_2.2.py
+++ Chapter-2/ch_2.2.py
@@ -1,1 +1,9 @@
+## DROP COLUMNS FORM A DATASET
 
+cols_to_remove = ['id', 'Date', 'living area', 'lot area']
+cols_to_remove = [col for col in cols_to_remove if col in df.columns]
+df2 = df.drop(cols_to_remove, axis=1)
+
+
+## REPLACE NaN 
+ - Rows having null values will be deleted
