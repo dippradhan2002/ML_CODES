ab7bcd11d42c1d45fbca16345c1461c6187bfecd
Dip Pradhan <117291092+dippradhan2002@users.noreply.github.com>
2024-06-30 18:27:41 +0530

    Update ch_2.1.py

--- Chapter-2/ch_2.1.py
+++ Chapter-2/ch_2.1.py
@@ -13,6 +13,9 @@ print(df.head())
 # Display the last few rows of the dataset
 print(df.tail())
 
+# Display random 3 rows of the dataset
+print(df.sample(3))
+
 #in the above two process we may not see all the rows and columns so better to do as below
 pd.set_option('display.max_columns',None)
 pd.set_option('display.max_rows',None)
