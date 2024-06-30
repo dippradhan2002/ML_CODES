701725bac84ab2b2cc691969e9e0f71a793913f8
Dip Pradhan <117291092+dippradhan2002@users.noreply.github.com>
2024-06-30 17:52:10 +0530

    Update ch_1.3.py

--- ch_1.3.py
+++ ch_1.3.py
@@ -1,1 +1,11 @@
+# Load the dataset
+df = pd.read_csv('your_dataset.csv')
 
+
+# Import data from google drive
+from google.colab import drive
+drive.mount('/content/drive')
+
+
+# Load dataset
+df = pd.read_csv('/content/drive/MyDrive/ML DATASET/cleaned_data.csv')
